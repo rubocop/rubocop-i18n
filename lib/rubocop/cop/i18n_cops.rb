cc7a682751cf00e69028880a894a645f85f45de2
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2025-01-15 16:30:00 -0500

    Reorganize requires

---
+++ lib/rubocop/cop/i18n_cops.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'i18n/gettext'
+require_relative 'i18n/gettext/decorate_string'
+require_relative 'i18n/gettext/decorate_function_message'
+require_relative 'i18n/gettext/decorate_string_formatting_using_interpolation'
+require_relative 'i18n/gettext/decorate_string_formatting_using_percent'
+
+require_relative 'i18n/rails_i18n'
+require_relative 'i18n/rails_i18n/decorate_string'
+require_relative 'i18n/rails_i18n/decorate_string_formatting_using_interpolation'
